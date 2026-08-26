bd6ad033b2ba629993885d3353c446489f17f790
Alharith Yassin <alhaymex@gmail.com>
2026-08-26 17:22:50 +0300

    bottom bar

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View } from "react-native"
+import { BottomBar } from "@/components/BottomBar"
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 bg-background">
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-foreground">Notes list</Text>
+      </View>
+
+      <BottomBar onSearchChange={() => {}} searchValue=""/>
     </View>
   )
 }
