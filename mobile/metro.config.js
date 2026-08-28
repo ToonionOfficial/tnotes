ddec2935eea1e1aaa7f0a4576204d1c5d66cec09
Alharith Yassin <alhaymex@gmail.com>
2026-08-28 19:32:48 +0300

    change metro config to use common js

--- mobile/metro.config.js
+++
@@ -1,9 +0,0 @@
-import { getDefaultConfig } from 'expo/metro-config.js'
-import { withUniwindConfig } from 'uniwind/metro'
-
-const config = getDefaultConfig(import.meta.dirname)
-
-export default withUniwindConfig(config, {
-  cssEntryFile: './global.css',
-  dtsFile: './src/uniwind-types.d.ts',
-})
