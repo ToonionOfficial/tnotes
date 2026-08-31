cc8fe73a5ca53b47fba61e6155ec5eff62052774
Alharith Yassin <alhaymex@gmail.com>
2026-08-31 11:35:33 +0300

    add the ast.rs

---
+++ crates/tnotes-document/src/ast.rs
@@ -0,0 +1,46 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Document {
+    pub blocks: Vec<Block>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub enum Block {
+    Paragraph(RichText),
+    Heading { level: u8, content: RichText },
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct RichText {
+    spans: Vec<Span>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Span {
+    pub text: String,
+    pub marks: Marks,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub link: Option<String>,
+}
+
+#[derive(Default, Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Marks {
+    pub bold: bool,
+    pub italic: bool,
+    pub underline: bool,
+    pub strike: bool,
+    pub code: bool,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct ListItem {
+    pub content: RichText,
+    pub children: Vec<ListItem>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct TaskItem {
+    pub checked: bool,
+    pub content: RichText,
+}
