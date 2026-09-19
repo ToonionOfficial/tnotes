1bb2def8e6b560b90e7f179804c9b940820eff85
Alharith Yassin <76082322+alhaymex@users.noreply.github.com>
2026-09-19 19:37:53 +0300

    Fix/twrite folding (#45)
    * update twrite and add issue_template, pr_template
    
    * update twrite and add issue_template, pr_template
    
    * bump version

--- apps/desktop/src/views/note_editor.rs
+++ apps/desktop/src/views/note_editor.rs
@@ -13,12 +13,12 @@ impl EditorHook for ContextMenuHook {
 pub fn build_note_editor(cx: &mut Context<Editor>) -> Editor {
     let mut ed = Editor::new("", cx);
 
-    ed.config.line_numbers = true;
     ed.theme.background = rgb(0x141318).into();
     ed.config.markdown.conceal_mode = ConcealMode::Hidden;
 
     ed.enable_markdown();
     ed.add_hook(SearchHook::new());
     ed.add_hook(ContextMenuHook);
+
     ed
 }
